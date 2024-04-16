db5b99d3b084e01c222cde4cd26add05a855c48c
AystorFullQuard <godunov2000@gmail.com>
2024-04-16 23:33:24 +0300

    123

--- Constants.py
+++ Constants.py
@@ -6,3 +6,4 @@ class Paths(Enum):
     project_path = os.path.dirname(os.path.realpath(__file__))
     test_images_path = os.path.join(project_path, 'test_images')
     results_path = os.path.join(project_path, 'results')
+    results_path_task1 = os.path.join(project_path, 'results/task1')
--- main.py
+++ main.py
@@ -3,24 +3,24 @@ from task1.ImageEnhancer import *
 
 
 def test_task1(image_path, image):
-    save_image(Paths.results_path.value, image, 'original_image_test.jpg')
+    save_image(Paths.results_path_task1.value, image, 'original_image_test.jpg')
 
     blurred_image = blur_image(image_path, kernel_size=(15, 15), sigmaX=20)
     show_two_images(image, blurred_image, 'Original Image', 'Blurred Image')
-    save_image(Paths.results_path.value, blurred_image, 'blurred_image_test.jpg')
+    save_image(Paths.results_path_task1.value, blurred_image, 'blurred_image_test.jpg')
 
     sharpened_image_1 = sharpen_image(image_path)
     show_two_images(image, sharpened_image_1, 'Original Image', 'Sharpened Image')
-    save_image(Paths.results_path.value, sharpened_image_1, 'sharpened_image_1_test.jpg')
+    save_image(Paths.results_path_task1.value, sharpened_image_1, 'sharpened_image_1_test.jpg')
 
     sharpened_image_2 = sharpen_image_alternative(image_path, 1)
     show_two_images(image, sharpened_image_2, 'Original Image', 'Sharpened Image alt')
-    save_image(Paths.results_path.value, sharpened_image_2, 'sharpened_image_2_test.jpg')
+    save_image(Paths.results_path_task1.value, sharpened_image_2, 'sharpened_image_2_test.jpg')
 
 
 if __name__ == '__main__':
     test_images_jpg_files = [f for f in os.listdir(Paths.test_images_path.value) if f.endswith('.jpg')]
-    image_path = Paths.test_images_path.value + '\\' + test_images_jpg_files[0]
+    image_path = Paths.test_images_path.value + '\\' + test_images_jpg_files[5]
     image = cv2.imread(image_path)
 
     test_task1(image_path, image)
